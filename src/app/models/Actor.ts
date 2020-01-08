e8f891ac638de6fe2531548ae4feaaa15753cbe8
ArturoAmoroso <art_ag7@hotmail.com>
2020-01-07 23:45:01 -0400

    Actors and movies /Models

---
+++ src/app/models/Actor.ts
@@ -0,0 +1,8 @@
+import { Movie } from './Movie'
+
+export class Actor {
+    name: string;
+    lasname: string;
+    age: number;
+    movies: Movie[];
+}
---
+++ src/app/models/Movie.ts
@@ -0,0 +1,6 @@
+export class Movie{
+    name: string;
+    duration: number;
+    genre: string;
+    // actorId: number;
+}
